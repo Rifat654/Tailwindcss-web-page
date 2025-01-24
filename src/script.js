8956e3ee221bbfb844b951aa8d60cdcfce85ff51
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-01-24 20:05:19 +0600

    make change on list items

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
+// nav bar responsive design start
 const bar = document.getElementById("btn");
 const ul = document.getElementById("nav-box");
 
 bar.addEventListener("click", () => {
   ul.classList.toggle("left-[0%]");
 });
+// nav bar responsive design end
